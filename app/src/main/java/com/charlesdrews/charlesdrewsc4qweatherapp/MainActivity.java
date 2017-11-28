9219c2c16dfaedb5c65a6f518fdef60f8ab74db2
charlesdrews <charlesdrews@gmail.com>
2017-11-28 10:40:21 -0500

    Add icons to project

--- app/src/main/java/com/charlesdrews/charlesdrewsc4qweatherapp/MainActivity.java
+++ app/src/main/java/com/charlesdrews/charlesdrewsc4qweatherapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.charlesdrews.charlesdrewsc4qweatherapp;
 
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +10,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +20,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        ImageView imageView = findViewById(R.id.image_view);
+        Drawable drawable;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            drawable = getDrawable(R.drawable.am_pcloudy);
+        } else {
+            drawable = getResources().getDrawable(R.drawable.am_pcloudy);
+        }
+        imageView.setImageDrawable(drawable);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
