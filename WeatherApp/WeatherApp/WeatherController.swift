5e555cb84a6df5131ddb5ea19fb3d2b5779af6d0
Sohailbasha <shlbsh94@gmail.com>
2018-07-12 13:32:54 -0400

    add assets

--- WeatherApp/WeatherApp/WeatherController.swift
+++ WeatherApp/WeatherApp/WeatherController.swift
@@ -5,6 +5,7 @@ class WeatherController {
     static let shared = WeatherController()
     let apiUrlString = "https://api.aerisapi.com/forecasts/11101?client_id=wmYzgqM9fPAGGxUROpWdY&client_secret=DwBMkf0OZoEGcKdgccIxGJFXdd6sdh3G89PEhqZY"
     
+    
     func fetchData() {
         guard let apiURL = URL(string: apiUrlString) else { return }
         
@@ -16,20 +17,18 @@ class WeatherController {
                     return
             }
             
-            guard let jsonDictionary = (try? JSONSerialization.jsonObject(with: data, options: .allowFragments)) as? [String:AnyObject] else { return }
+            guard let jsonDictionary = (try? JSONSerialization.jsonObject(with: data, options: .allowFragments)) as? [String:Any] else { return }
             
             
-            
-            guard let responseDictionary = jsonDictionary["response"] as? [[String:AnyObject]] else {
-                print("unable to serialize json. \nresponse: \(responseDataString)")
-                    return
-            }
+
+//            guard let responseDictionary = jsonDictionary["response"] as? [[String:AnyObject]] else {
+//                print("unable to serialize json. \nresponse: \(responseDataString)")
+//                    return
+//            }
             
             print(responseDataString)
-            print(responseDictionary)
+//            print(responseDictionary)
 
-            
-            
         }
     }
     
